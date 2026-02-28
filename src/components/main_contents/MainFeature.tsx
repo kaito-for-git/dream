2565ee51a1f40f6e079c935daeb1fdbcfa0a57ce
kaito <20001108kaito@gmail.com>
2026-02-28 14:58:19 +0900

    サイドバーから選択されたノートだけを表示するは完成したが、useStateの設計を検討する必要があるため一旦コミットする

--- src/components/main_contents/MainFeature.tsx
+++ src/components/main_contents/MainFeature.tsx
@@ -3,19 +3,26 @@ import {dummydate} from '../Data/dummydate1'
 import type { Note } from '../Features/Note'
 import { useState } from 'react'
 
+type Props ={
+    selectedId:string | null;
+}
+
 //メインの機能を描画する
-function MainFeature(){
-    const [note,setNote]  = useState<Note[]>([]);
+function MainFeature({selectedId}:Props){
+    //const [note,setNote]  = useState<Note[]>();
+    const selectNote = dummydate.find(s => s.id === selectedId);
     
     return(
-        <div className="MainFeature-main">
-            {/*ダミーデータ表示 */}
-            {dummydate.map((note:Note) =>(
-                <div key={note.id}>
-                    <h3>{note.title}</h3>
-                    <p>{note.content}</p>   
-                </div>
-            ))}
+        <div>
+            {selectNote ? (
+                <>
+                    <h3>{selectNote.title}</h3>
+                    <p>{selectNote.content}</p>
+                </>
+                ):(
+                    <p>サイドバーからノートを選択</p>
+                )
+            }    
         </div>
     )
 }
--- src/components/side_bar/SideBar.tsx
+++ src/components/side_bar/SideBar.tsx
@@ -3,38 +3,42 @@ import { useState } from 'react'
 import { dummydate } from '../Data/dummydate1';
 import type { Note } from '../Features/Note'
 import { data } from 'react-router-dom';
+import MainFeature from '../main_contents/MainFeature';
 
 //サイドバーの描画をする
 function SideBar(){
     const [isOpen,setIsOpen] = useState(false);//サイドバーの状態
-    const [note,setNotes] = useState<Note[]>(dummydate);//ノートの状態保存
+    const [note,setNotes] = useState<Note[]>(dummydate);//ノートの状態
+    const [selectedId,SetSelectedId] = useState<string | null>(null);//main featureに渡すための状態
 
     return(
+        <>
         <div className={`SideBar-Style ${isOpen ? 'open' : 'closed'}`}>
-            
             <button className="Hamburger-Style" onClick={() => setIsOpen(!isOpen)}>
                 ☰
             </button>
-            {isOpen &&
-                dummydate.map((note) =>(
-                    <button 
-                        key={note.id}
-                        className='Notes-Style' 
-                    >
-                        <div className="A-Note-style">
-                            <h4>{note.title}</h4>
-                            <small>
-                                {new Date(note.updated).toLocaleString('jp-JP',{
-                                    month: 'numeric',
-                                    day: '2-digit',
-                                    hour: '2-digit',
-                                    minute: '2-digit',
-                            })}</small>
-                        </div>
-                    </button>
-                ))
-            }
+            {dummydate.map((note) =>(
+                <button 
+                key={note.id}
+                className='Notes-Style' 
+                onClick={() => SetSelectedId(note.id)}//main featureに描画するノートのidを渡す
+                >
+                <div className="A-Note-style">
+                    <h4>{note.title}</h4>
+                    <small>
+                        {new Date(note.updated).toLocaleString('jp-JP',{
+                            month: 'numeric',
+                            day: '2-digit',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                        })}
+                    </small>
+                </div>
+            </button>
+            ))}
         </div>
+        <MainFeature selectedId={selectedId}/>
+        </>
     )
 }
 
