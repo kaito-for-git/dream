ac9eea842c8418c483c7aa70a9b2f11bd391d91b
kaito <20001108kaito@gmail.com>
2026-03-20 16:53:17 +0900

    ノートのCRUD設計API実装 #51

--- Back_End/schemas.py
+++ Back_End/schemas.py
@@ -12,6 +12,14 @@ class UserConfigBase(BaseModel):
     class Config:
         from_attributes = True # SQLAlchemyモデルをPydanticに変換可能にする
 
+# ==========================================
+# ノート機能 (Note)
+# - ノートベース    
+# - ノート新規作成    
+# - ノート更新  
+# - ノート取得
+# ==========================================
+
 # ノートベース
 class NoteConfigBase(BaseModel):
     user_id:str             #ユーザとの紐付け
@@ -24,3 +32,19 @@ class NoteConfigBase(BaseModel):
 
     class Config:
         from_attributes = True
+
+# ノート新規作成
+class NoteCreate(NoteConfigBase):
+    pass
+
+# ノート更新
+class NoteUpdate(BaseModel):
+    user_id:str
+    id:str
+    title: Optional[str] = None
+    content: Optional[str] = None
+    updated:int
+
+# ノート取得用
+class NoteConfig(NoteConfigBase):
+    pass
